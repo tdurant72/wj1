f76bc80079830e97696cba0c7315198d7dd0f8e5
Tyus Durant <contact@tyusdurant.com>
2023-06-14 10:00:59 -0400

    as of 0614

--- src/routes/(pages)/contact/+page.server.js
+++ src/routes/(pages)/contact/+page.server.js
@@ -1,12 +1,12 @@
 // import { fail } from '@sveltejs/kit';
 import { object, string } from 'yup';
+
 export const actions = {
 	default: async ({ request }) => {
 		const formData = await request.formData();
 		const name = formData.get('name');
 		const email = formData.get('email');
 		const message = formData.get('message');
-		// console.log('formData', name, email, message);
 
 		const contactFormSchema = object({
 			name: string().min(2, 'too short').required('We only accept named users'),
@@ -15,23 +15,18 @@ export const actions = {
 		});
 
 		try {
-			await contactFormSchema.validate(
-				{
-					name,
-					email,
-					message
-				},
-				{ abortEarly: false }
-			);
+			await contactFormSchema.validate({ name, email, message }, { abortEarly: false });
+
 			const prefilledLink = `https://docs.google.com/forms/d/e/1FAIpQLSetiqLPp4LqU2jS8OI6AhfpxHfpXczgurbkDIOLSBdpgE4KJw/formResponse?usp=pp_url&entry.2005620554=${name}&entry.1544096695=${email}&entry.839337160=${message}&submit=Submit`;
-			const res = await fetch(prefilledLink);
-			console.log('google form ', res);
+			// https://docs.google.com/forms/d/e/1FAIpQLSetiqLPp4LqU2jS8OI6AhfpxHfpXczgurbkDIOLSBdpgE4KJw/viewform?usp=pp_url&entry.2005620554=Tyus+Durant&entry.1544096695=todurant@sublimeux.com&entry.839337160=test
+			await fetch(prefilledLink);
+
 			return {
 				success: true,
-				status: 'Form has been submitted'
+				status: 'Form is submitted'
 			};
 		} catch (error) {
-			console.log('error', { error });
+			console.log({ error });
 			const errors = error.inner.reduce((acc, err) => {
 				return { ...acc, [err.path]: err.message };
 			}, {});
